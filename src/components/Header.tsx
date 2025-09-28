3339c5a595f8cbfcf0324224709262f34d4b90e5
devdussey <ukdussey@hotmail.com>
2025-09-27 18:59:30 -0600

    Rename application to Purge by DevDussey

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ export function Header() {
               className="h-10 w-10 rounded-lg"
             />
             <div>
-              <h1 className="text-xl font-bold text-white">Purge - Antivirus</h1>
+              <h1 className="text-xl font-bold text-white">Purge</h1>
               <p className="text-sm text-gray-400">by DevDussey</p>
             </div>
           </div>
